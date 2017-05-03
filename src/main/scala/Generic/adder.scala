f32b4c2ecddf7c9264ec7ca04ae26384fa3c7dfd
sirsleepy93 <sir.sleepy93@gmail.com>
2017-05-02 23:58:00 -0500

    Program Counter 1.0

--- src/main/scala/Generic/adder.scala
+++
@@ -1,9 +0,0 @@
-// Generic adder object that will be used as a component in several places.
-package pipeline
-
-import chisel3._
-
-object adder {
-	def wrap(num: UInt, max: UInt) =
-		Mux(num > max, 0.U, num)
-}
---
+++ src/main/scala/ProgramCounter.scala
@@ -0,0 +1,37 @@
+// Generic counter to be used as a component of the Program Counter.
+
+package pipeline
+
+import chisel3._
+
+class ProgCounter(width: Int) extends Module {
+	def wrap(count: UInt) =
+    	Mux(Bool(count.getWidth > width), 0.U, count)
+
+    def counter(clk: Bool, rst: Bool, set: Bool, setVal: UInt): UInt = {
+    //	val count = Reg(init=0.asUInt(width.W))
+		val count = Reg(init=0.U) // the vagueness seems problematic
+    	when (clk) {
+			when (rst) {
+				count := 0.U
+			} .elsewhen (set) {
+				count := wrap(setVal) // sure to cause some trouble down the line
+			} .otherwise {
+				count := wrap(count + 1.U)
+			}
+		}
+      	count
+    }
+
+	val io = IO(new Bundle {
+		val clk = Input(Bool())
+		val rst = Input(Bool())
+		val set = Input(Bool())
+		val setVal = Input(UInt(width.W))
+
+		val count = Output(UInt(width.W))
+	})
+
+	io.count := counter(io.clk, io.rst, io.set, io.setVal)
+
+}
